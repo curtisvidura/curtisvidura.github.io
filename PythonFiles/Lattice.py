eab539be569d66803c780271fe62bb2287e11372
Curtis McDowell <50535410+curtisvidura@users.noreply.github.com>
2024-10-19 21:55:13 +0700

    new gallery ai test

--- PythonFiles/Lattice.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-import latticegen
-
-lattice = latticegen.anylattice_gen(r_k=0.01, theta=0,
-                                    order=1, symmetry=4)
-plt.imshow(lattice.T)
